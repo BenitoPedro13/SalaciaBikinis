81df5441694e62e552fb07784cbae38ebc4c4625
1REZ3 <benitopedro21@outlook.com>
2021-08-16 19:14:00 -0300

    home page finalized

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,97 +1,118 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+    <v-app-bar flat absolute color="transparent" height="80" app hide-on-scroll>
+      <nuxt-link class="branco" to="/">
+        <v-toolbar-title v-text="title" />
+      </nuxt-link>
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+      <v-btn-toggle v-model="toggle_exclusive" mandatory group class="nav">
+        <nuxt-link to="/">
+          <v-btn text class="nav-menu">home</v-btn>
+        </nuxt-link>
+        <nuxt-link to="/inspire">
+          <v-btn text class="nav-menu">bikinis</v-btn>
+        </nuxt-link>
+        <nuxt-link to="/contato">
+          <v-btn text class="nav-menu">contato</v-btn>
+        </nuxt-link>
+        <nuxt-link to="/blog">
+          <v-btn text class="nav-menu">blog</v-btn>
+        </nuxt-link>
+      </v-btn-toggle>
     </v-app-bar>
+    <div class="social-line">
+      <v-btn text class="shop">
+        <svg width="51" height="36" viewBox="0 0 51 48" fill="none">
+          <path
+            d="M38.9848 41.1522L37.4151 14.8696C37.3497 13.5 36.1724 12.3913 34.799 12.3913H28.8473V9.58696C28.8473 4.30435 24.5307 0 19.233 0C13.9353 0 9.61874 4.30435 9.61874 9.58696V12.3913H3.66705C2.22818 12.3913 1.11633 13.5 1.05092 14.8696L0.00447167 41.2174C-0.0609315 42.9783 0.5931 44.6739 1.83576 45.9783C3.07842 47.2826 4.7789 48 6.54479 48H32.379C34.2103 48 35.9108 47.2826 37.1535 45.9783C38.3961 44.6739 39.1156 42.9783 38.9848 41.1522ZM12.3003 9.58696C12.3003 5.73913 15.4396 2.6087 19.2984 2.6087C23.1572 2.6087 26.2965 5.73913 26.2965 9.58696V12.3913H12.3003V9.58696ZM35.3222 44.2174C34.6027 45 33.5563 45.4565 32.4444 45.4565H6.61019C5.56374 45.4565 4.51729 45 3.79785 44.2826C3.07842 43.5 2.686 42.5217 2.686 41.413L3.73245 15.0652H9.68414V26.3478H12.3003V15.0652H26.2311V26.3478H28.8473V15.0652H34.799L36.3686 41.3478C36.434 42.3913 36.107 43.4348 35.3222 44.2174Z"
+            fill="white"
+          />
+          <circle cx="40.5" cy="10.5" r="10.5" fill="white" />
+          <path
+            d="M38.5046 14H42.8126V12.884H41.3486V5.6L38.5766 6.476V7.652L40.0166 7.244V12.884H38.5046V14Z"
+            fill="black"
+          />
+        </svg>
+      </v-btn>
+      <v-btn class="line"></v-btn>
+      <a target="_blank" href="https://www.instagram.com" class="insta"
+        >instagram</a
+      >
+      <a target="_blank" href="https://wa.me/5521991672783" class="wpp"
+        >whatsapp</a
+      >
+    </div>
     <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
+      <Nuxt />
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer :absolute="!fixed" app>
+      <div class="somos-salacia">
+        <p>nos somos</p>
+        <h1>salacia</h1>
+      </div>
+      <div class="conte">
+        <h1>
+          conte qualquer <br />
+          coisa pra gente.
+        </h1>
+      </div>
+      <div class="journal">fique atualizado sobre nossos produtos.</div>
+      <form action="">
+        <input id="nome" type="text" name="nome" placeholder="nome" />
+        <input id="email" type="email" name="email" placeholder="email" />
+        <v-btn text class="enviar"
+          >enviar
+          <svg
+            width="80"
+            height="16"
+            viewBox="0 0 106 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M0.999948 7.7169L104 8.01758"
+              stroke="white"
+              stroke-width="3"
+            />
+            <path
+              d="M104 8.01758L98.0171 2.00009"
+              stroke="white"
+              stroke-width="3"
+            />
+            <path
+              d="M104 8.01736L97.9822 13.9998"
+              stroke="white"
+              stroke-width="3"
+            />
+          </svg>
+        </v-btn>
+      </form>
+      <div class="policies">
+        <div class="copy">
+          © Salacia 2021. Todos os Direitos <br />
+          Reservados
+        </div>
+        <div class="terms">
+          <nuxt-link to="/servicos">
+            <v-btn text class="term">serviços</v-btn>
+          </nuxt-link>
+          <nuxt-link to="/institucional">
+            <v-btn text class="term">institucional</v-btn>
+          </nuxt-link>
+          <nuxt-link to="/cookies">
+            <v-btn text class="term">cookies</v-btn>
+          </nuxt-link>
+        </div>
+      </div>
     </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
+      toggle_exclusive: undefined,
       clipped: false,
       drawer: false,
       fixed: false,
@@ -110,8 +131,260 @@ export default {
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'salacia'
     }
   }
 }
 </script>
+
+<style scoped>
+header {
+  width: calc(100vw - 120px);
+}
+
+.v-toolbar__title {
+  mix-blend-mode: difference !important;
+  font-family: 'Nexa';
+  font-weight: 700;
+  font-size: 28px;
+}
+.nav {
+  width: 35%;
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+}
+
+.theme--light.v-btn--active:hover::before,
+.theme--light.v-btn--active::before {
+  opacity: 0 !important;
+}
+
+.nav-menu {
+  font-family: 'Nexa';
+  margin: 0 auto;
+  text-transform: lowercase !important;
+  font-weight: 300;
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  color: white !important;
+}
+.nuxt-link-exact-active .nav-menu {
+  font-weight: 700;
+}
+.nav-menu::after {
+  content: '';
+  display: none;
+  position: absolute;
+  width: 5px;
+  height: 5px;
+  background: currentColor;
+  border-radius: 50%;
+  top: 85%;
+}
+.nuxt-link-exact-active .nav-menu::after {
+  display: block;
+}
+
+.shop.v-btn::before {
+  background-color: transparent;
+}
+
+.social-line {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  width: 120px;
+  height: 776px;
+  left: 91.1%;
+  top: 22px;
+  z-index: 2;
+}
+
+button.v-btn.line {
+  content: '' !important;
+  display: block !important;
+  height: 362px !important;
+  min-width: 2px;
+  max-width: 2px !important;
+  width: 2px !important;
+  background: white;
+  padding: 0 !important;
+  margin: 30px 5px 0 0;
+}
+
+.wpp {
+  display: block;
+  font-family: 'Nexa';
+  font-style: normal;
+  font-weight: 300;
+  font-size: 24px;
+  line-height: 29px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: white;
+  transform: rotate(90deg);
+  margin: 100px 5px 0 0;
+}
+
+.insta {
+  display: block;
+  font-family: 'Nexa';
+  font-style: normal;
+  font-weight: 300;
+  font-size: 24px;
+  line-height: 29px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: white;
+  transform: rotate(90deg);
+  margin: 100px 5px 0 0;
+}
+
+.v-footer {
+  height: 600px;
+  background: #72c6d5;
+  position: relative;
+  padding: 0 100px;
+  justify-content: space-evenly;
+  align-items: flex-start;
+  flex-direction: column;
+}
+
+.somos-salacia h1,
+.somos-salacia p {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.somos-salacia p {
+  font-family: 'Nexa';
+  font-style: normal;
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 20px;
+  display: flex;
+  align-items: center;
+  color: white;
+}
+
+.somos-salacia h1 {
+  font-family: Nexa;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 32px;
+  line-height: 32px;
+  display: flex;
+  align-items: center;
+  color: white;
+}
+
+.conte {
+  font-family: 'Qaligo';
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 80px;
+  display: flex;
+  align-items: flex-end;
+  color: white;
+}
+.journal {
+  font-family: Nexa;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 28px;
+  line-height: 36px;
+  display: flex;
+  align-items: flex-end;
+  color: white;
+}
+form {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+}
+input {
+  margin-right: 30px;
+}
+form input {
+  font-weight: bold;
+  font-size: 32px;
+  line-height: 48px;
+  color: white;
+  outline: none;
+  opacity: 0.5;
+  border-bottom: 1px solid white;
+}
+form input:hover {
+  opacity: 1;
+  transition: 0.3s;
+}
+.enviar {
+  font-weight: bold;
+  font-size: 32px;
+  line-height: 48px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-transform: lowercase;
+  width: 250px !important;
+  color: white;
+}
+.enviar:hover svg {
+  animation: 0.7s enviar infinite;
+}
+@keyframes enviar {
+  0% {
+    transform: translateX(0);
+  }
+  50% {
+    transform: translateX(10px);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+.policies {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.copy {
+  font-family: 'Nexa';
+  font-style: normal;
+  font-weight: 300;
+  font-size: 18px;
+  line-height: 22px;
+  display: flex;
+  align-items: flex-end;
+  text-transform: uppercase;
+  color: white;
+}
+.terms {
+  font-family: 'Nexa';
+  font-style: normal;
+  font-weight: 300;
+  font-size: 18px;
+  line-height: 22px;
+  display: flex;
+  align-items: flex-end;
+  text-align: right;
+  text-transform: uppercase;
+  color: white;
+}
+.term {
+  font-family: 'Nexa';
+  font-weight: 300;
+  font-size: 18px;
+  line-height: 22px;
+  color: white;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,157 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
+  <div class="">
+    <section class="hero">
+      <v-img
+        src="/_nuxt/static/ana_deitada.jpeg"
+        lazy-src="/_nuxt/static/ana_deitada-min.jpeg"
+        class="hero-img"
+      >
+        <template #placeholder>
+          <v-row class="fill-height ma-0" align="center" justify="center">
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
+      <div class="content">
+        <v-btn text class="seta">
+          <svg
+            width="105"
+            height="14"
+            viewBox="0 0 105 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+            <path
+              d="M104.017 7.28317L1.01758 6.98248"
+              stroke="white"
+              stroke-width="1.5"
+            />
+            <path
+              d="M1.01758 6.98248L7.00004 13"
+              stroke="white"
+              stroke-width="1.5"
+            />
+            <path
+              d="M1.0174 6.98252L7.03489 1.00006"
+              stroke="white"
+              stroke-width="1.5"
+            />
+          </svg>
+        </v-btn>
+
+        <div class="cta">
+          <p>coleçao carnaval 2022</p>
+          <h2>vastidao, sol e mar</h2>
+          <nuxt-link to="/bikinis" class="link">ver bikinis</nuxt-link>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
+
+<style>
+@import url('http://fonts.cdnfonts.com/css/nexa-bold');
+@import url('http://fonts.cdnfonts.com/css/qaligo');
+
+html {
+  font-family: 'Nexa', 'Roboto';
+}
+main {
+  padding: 0px !important;
+}
+img {
+  display: block;
+  max-width: 100%;
+}
+a {
+  text-decoration: none;
+}
+.branco {
+  color: white !important;
+  mix-blend-mode: difference !important;
+}
+.preto {
+  color: black !important;
+  mix-blend-mode: exclusion !important;
+}
+
+.hero {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.hero .content {
+  position: absolute;
+  display: flex;
+  align-items: center;
+}
+.seta {
+  position: relative;
+  right: 300px;
+}
+.content .seta:hover {
+  animation: 0.7s enviar infinite;
+}
+@keyframes enviar {
+  0% {
+    transform: translateX(0);
+  }
+  50% {
+    transform: translateX(-10px);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+.hero .cta {
+  height: 220px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.cta p {
+  margin: 0;
+  font-family: 'Nexa';
+  font-style: normal;
+  font-weight: 300;
+  font-size: 32px;
+  line-height: 58px;
+  display: flex;
+  align-items: flex-end;
+  text-align: center;
+  color: white;
+}
+.cta h2 {
+  font-family: Qaligo;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 32px;
+  line-height: 70px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: white;
+}
+.cta .link {
+  color: white;
+  width: 280px;
+  height: 56px;
+  display: flex;
+  align-content: center;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  background: #72c6d5;
+  font-family: Nexa;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 24px;
+}
+</style>
